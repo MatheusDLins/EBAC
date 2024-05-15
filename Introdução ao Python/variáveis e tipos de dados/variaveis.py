60d5e4e68316f6984c0e42e057002cefed2ada52
MatheusDLins <matheus934qwer@gmail.com>
2024-05-15 13:54:17 -0300

    variaveis adicionadas

---
+++ Introdução ao Python/variáveis e tipos de dados/variaveis.py
@@ -0,0 +1,53 @@
+#Definição
+#Mecanismo de armazenamento volátil de dados, ou seja, dados salvos em pedacinhos da memória RAM do sistema computacional em uso (*notebook*, *mobile*, console de vídeo game, *smartwatch*, etc.).
+
+idade = 30
+print(idade)
+
+idade = 27
+print(idade)
+
+nome = "andré"
+print(nome)
+
+
+#Tipos nativos
+#Tipos numéricos: inteiros (int) e decimais (float):
+preco = 1000
+tipo_preco = type(preco)
+
+print(preco)
+print(tipo_preco)
+
+juros = 0.05
+tipo_juros = type(juros)
+
+print(juros)
+print(tipo_juros)
+
+
+#Tipos de texto: strings (str):
+
+primeiro_nome = "André"
+
+print(primeiro_nome)
+print(type(primeiro_nome))
+
+pais = 'Brasil'
+
+print(pais)
+print(type(pais))
+
+
+#Tipos lógicos: booleanos (bool):
+usuario_maior_de_idade = True
+
+print(usuario_maior_de_idade)
+print(type(usuario_maior_de_idade))
+
+
+#Tipo vazio (NoneType):
+telefone_fixo = None
+
+print(telefone_fixo)
+print(type(telefone_fixo))
