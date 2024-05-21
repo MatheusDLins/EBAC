8f325dbac36b37f25510c0890ab357da60e00fac
MatheusDLins <matheus934qwer@gmail.com>
2024-05-21 14:38:13 -0300

    arquivos e funções criados + leitura finalizado

---
+++ Introdução ao Python/arquivos e funções/leitura.py
@@ -0,0 +1,50 @@
+# Comando para ler arquivos.
+
+'''
+with open(file='<caminho do arquivo>', mode='<modo de leitura>', encoding='<decodificador>') as <apelido>:
+    bloco de código
+'''
+
+conteudo = None
+
+with open(file='./banco.csv', mode='r', encoding='utf8') as arquivo:
+    conteudo = arquivo.read()
+
+#print(conteudo)
+
+
+# readline
+# Comando para ler o conteúdo de um arquivo uma linha por vez.
+
+conteudo = []
+
+with open(file='./banco.csv', mode='r', encoding='utf8') as arquivo:
+    linha = arquivo.readline()  # lê a primeira linha
+    while linha:
+        conteudo.append(linha)
+        # lê uma nova linha, se a linha não existir salva o valor None
+        linha = arquivo.readline()
+
+# print(conteudo)
+
+'''for linha in conteudo:
+    print(linha)'''
+
+
+# Exemplo: Extraindo os valores da primeira coluna (idade).
+idades = []
+
+with open(file='./banco.csv', mode='r', encoding='utf8') as arquivo:
+    linha = arquivo.readline()  # lê o cabeçalho
+    linha = arquivo.readline()  # lê a primeira linha
+    while linha:
+        # quebra a string nas virgulas(ponto e virgula) e salva os resultados em uma lista
+        linha_separada = linha.split(sep=';')
+        idade = linha_separada[0]  # seleciona o primeiro elemento da lista
+        #idade = int(idade)  # converte o valor de string para integer (inteiro)
+        idades.append(idade)  # salva o valor na lista de idades
+        # lê uma nova linha, se a linha não existir, salva o valor None
+        linha = arquivo.readline()
+
+
+print(idades)
