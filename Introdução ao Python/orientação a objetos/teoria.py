247c401533471c342f5cd6f68e2462fa2cf68a38
MatheusDLins <matheus934qwer@gmail.com>
2024-06-04 15:59:17 -0300

    teoria finalizada

---
+++ Introdução ao Python/orientação a objetos/teoria.py
@@ -0,0 +1,20 @@
+#Classe
+'''
+Uma representação de um elemento, real ou não. Uma receita para criar objetos(instâncias)
+
+Exemplo: pessoa.
+'''
+
+#Objeto
+'''
+Uma instancia de uma classe. Dá vida a receita (classe)]
+
+Exemplo: André é uma instância da classe pessoa
+'''
+
+#Herança
+'''
+Uma especialização da classe.
+
+Exemplo: estudante é um tipo especial de pessoa.
+'''
