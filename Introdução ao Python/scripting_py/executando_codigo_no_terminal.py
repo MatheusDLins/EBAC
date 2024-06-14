8cbdff9427e29fc1c247c3bb2c332517553e3892
MatheusDLins <matheus934qwer@gmail.com>
2024-06-14 10:39:16 -0300

    terminal finalizado

---
+++ Introdução ao Python/scripting_py/executando_codigo_no_terminal.py
@@ -0,0 +1,13 @@
+'''
+1.   Criar uma pasta (sugestão);
+2.   Abrir o terminal nesta pasta (windows `shift + right click` ou linux/macos `right click`);
+3.   Executar o seguinte comando:
+
+python
+
+
+Para 'sair' do Python, basta fechar o terminal ou digitar o seguinte código:
+
+quit()
+
+'''
