15990cc2e6838420f05110ccd73d9c4571d7b8ae
MatheusDLins <matheus934qwer@gmail.com>
2024-06-14 09:39:46 -0300

    instalação local concluida

---
+++ Introdução ao Python/scripting_py/instalando_python_na_maquina.py
@@ -0,0 +1,12 @@
+'''
+1.2. Máquina Local
+
+Verifique se você já tem o Python e o PIP instalado na sua máquina. Para verificar a instalação, abrir o terminal (windows `cmd` ou linux/macos `terminal`) e executar o seguinte comando.
+
+
+python -V
+pip -V
+
+
+Se não o tiver instalado, acessar o site oficial do Python ([link](https://www.python.org/)) e instalar o software para o seu sistema operacional. Um excelente tutorial se encontra neste [link](https://realpython.com/installing-python/). Para verificar a instalação, abrir o terminal (windows `cmd` ou linux/mcos `terminal`) e executar o seguinte comando.
+'''
