42c20daa3f6203fd438b771999c83d0bce2430b8
MatheusDLins <matheus934qwer@gmail.com>
2024-08-13 11:06:39 -0300

    condicionais

---
+++ Front-End/Fundamentos do JavaScript/condicionais.js
@@ -0,0 +1,12 @@
+let idade = 22
+
+if(idade >= 16){
+    console.log('Tudo certo!');
+} else {
+    console.log('Menor de idade');
+}
+
+
+const resultado = idade >= 16 ? 'certo!' : 'Menor de idade'
+
+console.log(resultado);
