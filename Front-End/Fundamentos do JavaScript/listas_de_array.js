ad90766484406e1f1243c37e0dcf68c6f9c0b00d
MatheusDLins <matheus934qwer@gmail.com>
2024-08-13 10:48:46 -0300

    lista e repetições

---
+++ Front-End/Fundamentos do JavaScript/listas_de_array.js
@@ -0,0 +1,11 @@
+const listaDeMateriais = ['HTML', 'CSS', 'JavaScript']
+
+console.log(listaDeMateriais);
+
+console.log(listaDeMateriais[1]);
+
+console.log(listaDeMateriais.length);
+
+listaDeMateriais.push('jQuery')
+
+console.log(listaDeMateriais);
---
+++ Front-End/Fundamentos do JavaScript/repeticoes_lacos.js
@@ -0,0 +1,5 @@
+let nomes = ['Maria', 'José', 'Jõao']
+
+for(let i = 0; i < nomes.length; i++){
+    console.log("O nome é: " + nomes[i]);
+}
