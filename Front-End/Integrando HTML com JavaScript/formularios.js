9257417390d3097804b5bb7b5bf80e1d22da049a
MatheusDLins <matheus934qwer@gmail.com>
2024-08-14 10:07:38 -0300

    manipulando formulários

---
+++ Front-End/Integrando HTML com JavaScript/formularios.js
@@ -0,0 +1,7 @@
+let nomeBeneficiario = document.getElementById('nome-beneficiario')
+
+nomeBeneficiario.value = "matheus"
+
+let valorDeposito = document.getElementById('valor-deposito')
+
+valorDeposito.value = 1
