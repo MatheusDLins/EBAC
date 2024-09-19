1312de2329785337ac12f4872024c92b5527201c
MatheusDLins <matheus934qwer@gmail.com>
2024-09-19 12:30:07 -0300

    Funcionalidade de nova imagem concluida

--- Front-End/Conheça o jQuery/main.js
+++ Front-End/Conheça o jQuery/main.js
@@ -9,5 +9,17 @@ $(document).ready(function(){
 
     $('form').on('submit',function(e){
         e.preventDefault();
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
+        console.log(enderecoDaNovaImagem)
+        const novoItem = $('<li style="display: none"></li>');
+        $(`<img src="${enderecoDaNovaImagem}"/>`).appendTo(novoItem);
+        $(`
+            <div class="overlay-imagem-link">
+                <a href="${enderecoDaNovaImagem}" title="Ver imagem em tamanho real" target="_blank">Ver imagem em tamanho real</a>
+            </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000);
+        $('#endereco-imagem-nova').val('')
     })
 })
